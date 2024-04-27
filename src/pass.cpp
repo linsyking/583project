3655237d93621f1d4a47af38f92cdf980c0d7cd0
Jiezhong Yang <pigumar@umich.edu>
2024-04-27 16:20:57 -0400

    auto formatted pass

--- src/pass.cpp
+++ src/pass.cpp
@@ -25,119 +25,129 @@
 
 using namespace llvm;
 
-namespace {
-struct VECPass : public PassInfoMixin<VECPass> {
-    PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
-        llvm::LoopAnalysis::Result &li = FAM.getResult<LoopAnalysis>(F);
-        IRBuilder<>                 builder(F.getContext());
-        std::vector<CallInst *>     MaskedLoadInstructions;
+namespace
+{
+    struct VECPass : public PassInfoMixin<VECPass>
+    {
+        PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM)
+        {
+            llvm::LoopAnalysis::Result &li = FAM.getResult<LoopAnalysis>(F);
+            IRBuilder<> builder(F.getContext());
+            std::vector<CallInst *> MaskedLoadInstructions;
 
-        for (auto &B : F) {
-            for (auto &I : B) {
-                if (auto *CI = dyn_cast<CallInst>(&I)) {
-                    auto *maskedFunction = CI->getCalledFunction();
-                    if (maskedFunction &&
-                        maskedFunction->getName().starts_with("llvm.masked.load")) {
-                        MaskedLoadInstructions.push_back(CI);
+            for (auto &B : F)
+            {
+                for (auto &I : B)
+                {
+                    if (auto *CI = dyn_cast<CallInst>(&I))
+                    {
+                        auto *maskedFunction = CI->getCalledFunction();
+                        if (maskedFunction &&
+                            maskedFunction->getName().starts_with("llvm.masked.load"))
+                        {
+                            MaskedLoadInstructions.push_back(CI);
+                        }
                     }
                 }
             }
+
+            for (auto *CI : MaskedLoadInstructions)
+            {
+                // Step 1: Generate unmasked load
+                BasicBlock *BB = CI->getParent();
+                builder.SetInsertPoint(CI);
+                // Step 2.1: Cast mask into integer
+
+                auto DL = DataLayout(F.getParent());
+                auto *mask = CI->getArgOperand(2);
+                auto *maskTy = mask->getType();
+                auto numBits = DL.getTypeSizeInBits(maskTy);
+
+                // I think we should use getTypeSizeInBits instead of getTypeAllocSizeInBits
+                // outs() << "Bits: " << DL.getTypeAllocSizeInBits(maskTy) << '\n';
+                // outs() << "Bits: " << DL.getTypeSizeInBits(maskTy) << '\n';
+
+                auto *intTy = IntegerType::get(maskTy->getContext(), numBits);
+                auto *castInstr = builder.CreateBitCast(CI->getArgOperand(2), intTy);
+                // Step 2.2: Guard the load with branch
+
+                auto *cmpInst =
+                    builder.CreateICmpEQ(castInstr, ConstantInt::get(castInstr->getType(), 0));
+                auto *branchIns = llvm::SplitBlockAndInsertIfElse(cmpInst, CI, false);
+                builder.SetInsertPoint(branchIns);
+                auto *unmaskedLoad =
+                    builder.CreateLoad(CI->getCalledFunction()->getReturnType(), CI->getArgOperand(0));
+                CI->replaceAllUsesWith(unmaskedLoad);
+                CI->eraseFromParent();
+                // auto *branchBB = BB->splitBasicBlock(dyn_cast<Instruction>(cmpInst));
+            }
+
+            // for (auto *B : BasicBlocks) {
+            //     for (auto &I : *B) {
+            //         if (auto *CI = dyn_cast<CallInst>(&I)) {
+            //             auto *maskedFunction = CI->getCalledFunction();
+            //             if (maskedFunction &&
+            //                 // Step 1: Generate unmasked load
+            //                 maskedFunction->getName().starts_with("llvm.masked.load")) {
+            //                 builder.SetInsertPoint(CI);
+            //                 auto *unmaskedLoad =
+            //                 builder.CreateLoad(maskedFunction->getReturnType(),
+            //                                                         CI->getArgOperand(0));
+            //                 CI->replaceAllUsesWith(unmaskedLoad);
+
+            //                 // Step 2.1: Cast mask into integer
+            //                 auto *DL      = new DataLayout(F.getParent());
+            //                 auto *mask    = CI->getArgOperand(2);
+            //                 auto *maskTy  = mask->getType();
+            //                 auto  numBits = DL->getTypeAllocSizeInBits(maskTy);
+            //                 auto *intTy   = IntegerType::get(maskTy->getContext(), numBits);
+            //                 // outs() << *CI->getArgOperand(2)->getType() << " TO " << *intTy <<
+            //                 // '\n';
+            //                 auto *castInst =
+            //                     new BitCastInst(CI->getArgOperand(2), intTy, "", unmaskedLoad);
+
+            //                 // // Step 2.2: Guard the load with branch
+            //                 // auto  cmpInst = new ICmpInst(unmaskedLoad, CmpInst::ICMP_EQ,
+            //                 // castInst,
+            //                 //                              ConstantInt::get(castInst->getType(),
+            //                 //                              0),
+            //                 //                              "");
+            //                 // auto *newBB   = B.splitBasicBlock(cmpInst);
+            //                 // auto *trueBB  = BasicBlock::Create(F.getContext(), "", &F, newBB);
+            //                 // auto  brInst  = BranchInst::Create(trueBB, newBB, cmpInst, &B);
+            //                 // B.getTerminator()->eraseFromParent();
+
+            //                 // // Step 3: Create select instruction
+            //                 // auto selectInst = SelectInst::Create(
+            //                 //     mask, unmaskedLoad, UndefValue::get(unmaskedLoad->getType()),
+            //                 "",
+            //                 //     CI);
+
+            //                 // // Step 4: Remove masked load
+            //                 // CI->removeFromParent();
+            //                 // return PreservedAnalyses::none();
+            //             }
+            //         }
+            //     }
+            // }
+
+            return PreservedAnalyses::none();
         }
+    };
 
-        for (auto *CI : MaskedLoadInstructions) {
-            // Step 1: Generate unmasked load
-            BasicBlock *BB = CI->getParent();
-            builder.SetInsertPoint(CI);
-            // Step 2.1: Cast mask into integer
+} // namespace
 
-            auto  DL      = DataLayout(F.getParent());
-            auto *mask    = CI->getArgOperand(2);
-            auto *maskTy  = mask->getType();
-            auto  numBits = DL.getTypeSizeInBits(maskTy);
-
-            // I think we should use getTypeSizeInBits instead of getTypeAllocSizeInBits
-            // outs() << "Bits: " << DL.getTypeAllocSizeInBits(maskTy) << '\n';
-            // outs() << "Bits: " << DL.getTypeSizeInBits(maskTy) << '\n';
-
-            auto *intTy = IntegerType::get(maskTy->getContext(), numBits);
-            auto *castInstr = builder.CreateBitCast(CI->getArgOperand(2), intTy);
-            // Step 2.2: Guard the load with branch
-
-            auto *cmpInst =
-                builder.CreateICmpEQ(castInstr, ConstantInt::get(castInstr->getType(), 0));
-            auto *branchIns = llvm::SplitBlockAndInsertIfElse(cmpInst, CI, false);
-            builder.SetInsertPoint(branchIns);
-            auto *unmaskedLoad =
-                builder.CreateLoad(CI->getCalledFunction()->getReturnType(), CI->getArgOperand(0));
-            CI->replaceAllUsesWith(unmaskedLoad);
-            CI->eraseFromParent();
-            // auto *branchBB = BB->splitBasicBlock(dyn_cast<Instruction>(cmpInst));
-        }
-
-        // for (auto *B : BasicBlocks) {
-        //     for (auto &I : *B) {
-        //         if (auto *CI = dyn_cast<CallInst>(&I)) {
-        //             auto *maskedFunction = CI->getCalledFunction();
-        //             if (maskedFunction &&
-        //                 // Step 1: Generate unmasked load
-        //                 maskedFunction->getName().starts_with("llvm.masked.load")) {
-        //                 builder.SetInsertPoint(CI);
-        //                 auto *unmaskedLoad =
-        //                 builder.CreateLoad(maskedFunction->getReturnType(),
-        //                                                         CI->getArgOperand(0));
-        //                 CI->replaceAllUsesWith(unmaskedLoad);
-
-        //                 // Step 2.1: Cast mask into integer
-        //                 auto *DL      = new DataLayout(F.getParent());
-        //                 auto *mask    = CI->getArgOperand(2);
-        //                 auto *maskTy  = mask->getType();
-        //                 auto  numBits = DL->getTypeAllocSizeInBits(maskTy);
-        //                 auto *intTy   = IntegerType::get(maskTy->getContext(), numBits);
-        //                 // outs() << *CI->getArgOperand(2)->getType() << " TO " << *intTy <<
-        //                 // '\n';
-        //                 auto *castInst =
-        //                     new BitCastInst(CI->getArgOperand(2), intTy, "", unmaskedLoad);
-
-        //                 // // Step 2.2: Guard the load with branch
-        //                 // auto  cmpInst = new ICmpInst(unmaskedLoad, CmpInst::ICMP_EQ,
-        //                 // castInst,
-        //                 //                              ConstantInt::get(castInst->getType(),
-        //                 //                              0),
-        //                 //                              "");
-        //                 // auto *newBB   = B.splitBasicBlock(cmpInst);
-        //                 // auto *trueBB  = BasicBlock::Create(F.getContext(), "", &F, newBB);
-        //                 // auto  brInst  = BranchInst::Create(trueBB, newBB, cmpInst, &B);
-        //                 // B.getTerminator()->eraseFromParent();
-
-        //                 // // Step 3: Create select instruction
-        //                 // auto selectInst = SelectInst::Create(
-        //                 //     mask, unmaskedLoad, UndefValue::get(unmaskedLoad->getType()),
-        //                 "",
-        //                 //     CI);
-
-        //                 // // Step 4: Remove masked load
-        //                 // CI->removeFromParent();
-        //                 // return PreservedAnalyses::none();
-        //             }
-        //         }
-        //     }
-        // }
-
-        return PreservedAnalyses::none();
-    }
-};
-
-}  // namespace
-
-extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK llvmGetPassPluginInfo() {
-    return {LLVM_PLUGIN_API_VERSION, "VECPass", "v0.1", [](PassBuilder &PB) {
+extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK llvmGetPassPluginInfo()
+{
+    return {LLVM_PLUGIN_API_VERSION, "VECPass", "v0.1", [](PassBuilder &PB)
+            {
                 PB.registerPipelineParsingCallback([](StringRef Name, FunctionPassManager &FPM,
-                                                      ArrayRef<PassBuilder::PipelineElement>) {
+                                                      ArrayRef<PassBuilder::PipelineElement>)
+                                                   {
                     if (Name == "vecpass") {
                         FPM.addPass(VECPass());
                         return true;
                     }
-                    return false;
-                });
+                    return false; });
             }};
 }
